725bf7262141f84fe8621f911d7ddbf10458888a
Thomas Renaud <thomasre@bam.tech>
2025-04-06 11:55:09 +0200

    chore: add basic configuration for tailwindcss to the frontend project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = {
+  primary: {
+    100: "#D4DBFF",
+    300: "#8698FD",
+    500: "#3A58FF",
+    700: "#0B26BA",
+    900: "#0A1863",
+  },
+};
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: "./postcss.config.js",
+  },
   test: {
     globals: true,
     environment: "jsdom",
