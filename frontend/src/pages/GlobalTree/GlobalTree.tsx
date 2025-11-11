bfc0e8b7434b68c30bc98b54de6fce67c8296adb
BlueSetbyeol <setbyeol.c@gmail.com>
2025-11-11 10:53:07 +0100

    corrections of sum of children's value

--- frontend/src/pages/GlobalTree/GlobalTree.tsx
+++ frontend/src/pages/GlobalTree/GlobalTree.tsx
@@ -49,15 +49,14 @@ export default function GlobalTree({
     }
   }, [currentBranch]);
 
-  const [childValueTotalWithYear, setChildValueTotalWithYear] = useState(0);
   const [childrenTotalValues, setChildrenTotalValues] = useState<
     [number, number][]
   >([]);
+  const [childValueTotalWithYear, setChildValueTotalWithYear] = useState(0);
 
   useEffect(() => {
     if (currentBranch.children !== undefined) {
       let totalValue = 0;
-      const gatheringChildrenValue: [number, number][] = [];
       for (let i = 0; i < currentBranch.children[0].values.length; i++) {
         totalValue = 0;
         const thatYear = currentBranch.children[0].values[i][0];
@@ -70,18 +69,15 @@ export default function GlobalTree({
           }
         });
         totalValue = Number(totalValue.toFixed(2));
-        gatheringChildrenValue.push([thatYear, totalValue]);
+        childrenTotalValues.push([thatYear, totalValue]);
       }
-      setChildrenTotalValues(gatheringChildrenValue);
-      // Ce qui suit est à supprimer quand cela aura été remplacé sans tout casser
-      currentBranch.children.forEach((element) => {
-        const childValue = element.values.find((info) => info[0] === isYear);
-        if (childValue) {
-          totalValue = totalValue + childValue[1];
-        }
-      });
-      totalValue = Number(totalValue.toFixed(2));
-      setChildValueTotalWithYear(totalValue);
+      setChildrenTotalValues(childrenTotalValues);
+      //récupération d'une seule valeur pour l'année demandée
+      const childValue = childrenTotalValues.find((info) => info[0] === isYear);
+      if (childValue) {
+        totalValue = Number(childValue[1].toFixed(2));
+        setChildValueTotalWithYear(totalValue);
+      }
     }
   }, [currentBranch, isYear]);
 
@@ -124,6 +120,7 @@ export default function GlobalTree({
   };
 
   const [lineChartToShow, setLineChartToShow] = useState<topicBranch>();
+  console.log(childValueTotalWithYear, childrenTotalValues);
 
   return (
     isYear !== 0 &&
@@ -216,7 +213,7 @@ export default function GlobalTree({
           >
             {currentBranch.id.length > 15 &&
             hasValue > 0 &&
-            childValueTotalWithYear > 0 &&
+            // childValueTotalWithYear > 0 && >> à vérifier
             !currentBranch.children[0].unit.includes("/") ? (
               <article className="camembert_chart">
                 <PieCharts isYear={isYear} currentBranch={currentBranch} />
@@ -231,9 +228,6 @@ export default function GlobalTree({
               <p></p>
             )}
             <article className="listed_children">
-              {/* {isYear === 10 && !currentBranch.id.includes("0_") && (
-                <p>Merci de choisir une année</p>
-              )} */}
               {currentBranch.children
                 .map((child) => ({
                   ...child,
