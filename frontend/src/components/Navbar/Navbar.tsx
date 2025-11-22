02d5ed376523424973c12cb68f4a2f33e3433544
BlueSetbyeol <128409673+BlueSetbyeol@users.noreply.github.com>
2025-11-22 18:50:41 +0100

    level up in CSS (#29)

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar({
             setShowLineChart(false);
           }}
         >
-          <h1>Logo</h1>
+          <h1>Numelis</h1>
         </button>
         <aside>
           <BrowsingDrawer
